b758c444d7f110b5307bbcff30814c0322859b16
Caio de Souza Santana <caiosantana.lkd@gmail.com>
2026-03-24 14:53:23 -0300

    Projeto crud-clients

---
+++ src/main/java/com/csantana/clientes/controllers/ClientController.java
@@ -0,0 +1,57 @@
+package com.csantana.clientes.controllers;
+
+import com.csantana.clientes.dto.ClientDTO;
+import com.csantana.clientes.services.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+    @RequestMapping(value = "/clients")
+    public class ClientController {
+
+        @Autowired
+        private ClientService service;
+
+        @GetMapping
+        public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+            Page<ClientDTO> list = service.findAllPaged(pageable);
+            return ResponseEntity.ok().body(list);
+        }
+
+        @GetMapping(value = "/{id}")
+        public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+
+            ClientDTO dto = service.findById(id);
+            return ResponseEntity.ok(dto);
+        }
+
+        @PostMapping
+        public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+            dto = service.insert(dto);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(dto.id()).toUri();
+            return ResponseEntity.created(uri).body(dto);
+        }
+
+        @PutMapping(value = "/{id}")
+        public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO dto) {
+
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+        @DeleteMapping(value = "/{id}")
+        public ResponseEntity<Void> update(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    }
+
---
+++ src/main/java/com/csantana/clientes/dto/CustomError.java
@@ -0,0 +1,6 @@
+package com.csantana.clientes.dto;
+
+import java.time.Instant;
+
+public record CustomError(Instant timestamp, Integer status, String error, String path) {
+}
